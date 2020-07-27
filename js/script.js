cbdd81ca1d735883af35504c692cfb9c7e8afe00
Mariusz Jarymowicz <mariusz_jarymowicz@tlen.pl>
2020-07-27 09:20:30 +0200

    add computer and player move to js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,20 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
 var playerInput, playerMove;
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Wpisana odpowiedź to: ' + playerInput);
