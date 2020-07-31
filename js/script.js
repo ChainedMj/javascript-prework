f63b4ad1b7fbaddcb5fdc0ad54bdc52f522f96e6
Mariusz Jarymowicz <mariusz_jarymowicz@tlen.pl>
2020-07-31 20:51:41 +0200

    working paper rock scissors game

--- js/script.js
+++ js/script.js
@@ -7,25 +7,22 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-var argButtonName, buttonPaper, buttonRock, buttonScissors, buttonTest;
 
-/**
- * Describe this function...
- */
 function buttonClicked(argButtonName) {
   clearMessages();
-  console.log(argButtonName + ' został kliknięty');
-}
-buttonTest = document.getElementById('button-test');
-buttonTest.addEventListener('click', function(){ buttonClicked('Guzik TEST'); });
-buttonRock = document.getElementById('button-rock');
-buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
-buttonPaper = document.getElementById('button-paper');
-buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
-buttonScissors = document.getElementById('button-scissors');
-buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
 
-var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
+  var randomNumber = Math.floor(Math.random() * 3 + 1);
+  var computerMove = getMoveName(randomNumber)
+  
+  displayResult(argButtonName, computerMove);
+}
+
+var buttonRock = document.getElementById('button-rock');
+buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
+var buttonPaper = document.getElementById('button-paper');
+buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
+var buttonScissors = document.getElementById('button-scissors');
+buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
 
 /**
  * Describe this function...
@@ -48,25 +45,15 @@ function getMoveName(argMoveId) {
  * Describe this function...
  */
 function displayResult(argPlayerMove, argComputerMove) {
-  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+  if ((argPlayerMove == 'papier' && argComputerMove == 'kamień')
+       || ((argPlayerMove == 'nożyce') && (argComputerMove == 'papier'))
+       || ((argPlayerMove == 'kamień') && (computerMove == 'nożyce'))) {
     printMessage('Wygrywasz!');
-  } else if ((argPlayerMove == 'nożyce') && (argComputerMove == 'papier')) {
-    printMessage('wygrywasz');
-  } else if ((argPlayerMove == 'kamień') && (computerMove == 'nożyce')) {
-    printMessage('wygrywasz');
   } else if (argPlayerMove == computerMove) {
     printMessage('remis');
+  } else {
+    printMessage('przegywasz')
   }
 
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
-
-console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = argButtonName;
-console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
